afd24aee24c5249e01804ca6aa8a58a6dfbf5098
Vonetta Stevenson <vonettastevenson@gmail.com>
2017-10-15 02:47:14 -0700

    todo list in jquery

---
+++ README.md
@@ -0,0 +1,2 @@
+"# todo-list"  git init git add README.md git commit -m "first commit" git remote add origin https://github.com/vonetta/todo-list.git git push -u origin master
+"# todo-list" 
---
+++ todo-list/assets/js/script.js
@@ -0,0 +1,25 @@
+﻿//check off specific todos by clicking
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation(); //stops bubbling up to other elements
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        console.log("you hit enter");
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li> ");
+    };
+})
+
+$(".fa-pencil-square-o").on("click", function () {
+    $("input[type='text']").fadeToggle();
+})
+
